0344fc23c69f4747d3018241ba8033e2bdc91969
aurelx03 <aurelx03@hotmail.fr>
2017-06-19 22:33:31 +0200

    Update and rename .gitignore to .test.lua

---
+++ .test.lua
@@ -0,0 +1,87 @@
+local horizontalCharSpacing = 6
+local verticalCharSpacing   = 10
+local x = 0
+local y = 0
+local tableau = {0, 0, 0, 0}
+local nbtour = 0
+local buffer = 0
+local score = 0
+
+local function DrawScreen()
+  lcd.clear()
+  lcd.drawFilledRectangle(0, 0, LCD_W, verticalCharSpacing)
+end
+
+local function DrawChrono()
+			lcd.drawTimer(x+20, y+30, tableau[1], MIDSIZE)
+ 			lcd.drawTimer(x+20, y+45, tableau[2], MIDSIZE)
+ 			lcd.drawTimer(x+70, y+30, tableau[3], MIDSIZE)
+ 			lcd.drawTimer(x+70, y+45, tableau[4], MIDSIZE)
+ 			lcd.drawText(5,10,nbtour)
+ 			--lcd.drawText(5,20,buffer)
+end
+
+local function Chrono()
+local race = model.getTimer(0) --getChrono Race
+local tour = model.getTimer(1) --getChrono Tour
+	if tour.value < 1 and race.value > 5 and nbtour < 1 then
+		tableau[1] = race.value
+		nbtour = 1
+	end
+	if tour.value < 1 and race.value > 1 and nbtour == 1 and buffer > 150 then
+		tableau[2] = race.value - tableau[1]
+		nbtour = 2
+		buffer = 0
+	end
+	if tour.value < 1 and race.value > 1 and nbtour == 2 and buffer > 150 then
+		tableau[3] = race.value - (tableau[1]+tableau[2])
+		nbtour = 3
+		buffer = 0
+	end
+	if tour.value < 1 and race.value > 1 and nbtour == 3 and buffer > 150 then
+		tableau[4] = race.value - (tableau[1]+tableau[2]+tableau[3])
+		nbtour = 4
+		buffer = 0
+	end
+buffer = buffer + 1
+end
+
+local function BestLap()
+	if tableau[1] < tableau[2] and tableau[1] < tableau[3] and tableau[1] < tableau[4] then
+		score = 1
+	end
+	if tableau[2] < tableau[1] and tableau[2] < tableau[3] and tableau[2] < tableau[4] then 
+		score = 2
+	end
+	if tableau[3] < tableau[1] and tableau[3] < tableau[2] and tableau[3] < tableau[4] then
+		score = 3
+	end
+	if tableau[4] < tableau[1] and tableau[4] < tableau[2] and tableau[4] < tableau[3] then
+		score = 4
+	end
+	if nbtour == 4 then
+		lcd.drawTimer(47,15,tableau[score], MIDSIZE+INVERS+BLINK)
+	end
+	tour = model.getTimer(1)
+	race = model.getTimer(0)
+	if tour.value < 1 and race.value < 2 then
+			tableau[1] = 0
+			tableau[2] = 0
+			tableau[3] = 0
+			tableau[4] = 0
+			score = 0
+			nbtour = 0
+			buffer = 0
+	end
+end
+
+
+local function Run(event)
+Chrono()
+DrawScreen()
+DrawChrono()
+BestLap()	 
+  return 0
+end
+
+return {init=InitUi, run=Run}
